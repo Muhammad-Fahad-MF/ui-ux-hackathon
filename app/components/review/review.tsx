741f2578a963a4a2b2aec2f3a47d388dc5970b0b
Muhammad-Fahad-MF <imperial.king42@gmail.com>
2024-12-15 23:37:46 +0500

    logo error

--- app/components/review/review.tsx
+++ app/components/review/review.tsx
@@ -4,7 +4,8 @@ export default function Review() {
     <div className="px-[300px] flex flex-col font-Inter400 text-white bg-black/95 py-[120px]">
       <h3  className="font-GreatVibes text-[32px] leading-[40px] text-primary1 ml-1">Testimonials</h3>
       <h1 className="font-HelveticaBold text-[48px] leading-[56px] mt-2">What our client are saying</h1>
-      <ReviewCard/>  
+      <ReviewCard/>
+        
     </div>
   )
 }
--- app/components/review/reviewCard/reviewCard.tsx
+++ app/components/review/reviewCard/reviewCard.tsx
@@ -6,6 +6,7 @@ export default function ReviewCard() {
     <div  style={{boxShadow: '0px 0px 80px 0px #CDCDCD40'}} className="bg-white w-[869px] h-[451px] mt-[123px] relative flex flex-col items-center">
         <Image src="/images/reviews/Dp.svg" alt="_" height={134} width={133} className="absolute mx-auto -top-[67px] rounded-full"/>
         <FaStar/>
+        <h3>hello hello</h3>
     </div>
   )
 }
